bbc6c7a0558bd8b7647ee82794ec1fe47e2c88a7
gracedorgan <gracedorgan@gmail.com>
2020-09-21 12:51:22 -0400

    just realized I committed the fully complete version, I am rewinding.

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,40 +1,10 @@
 import React from 'react';
 
+// This component is where the user can type their message and send it
+// to the chat room. We shouldn't communicate with the server here though.
 class ChatInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { chatInput: '' };
-
-    // React ES6 does not bind 'this' to event handlers by default
-    this.submitHandler = this.submitHandler.bind(this);
-    this.textChangeHandler = this.textChangeHandler.bind(this);
-  }
-  
-  submitHandler(event) {
-    // Stop the form from refreshing the page on submit
-    event.preventDefault();
-
-    // Clear the input box
-    this.setState({ chatInput: '' });
-
-    // Call the onSend callback with the chatInput message
-    this.props.onSend(this.state.chatInput);
-  }
-
-  textChangeHandler(event)  {
-    this.setState({ chatInput: event.target.value });
-  }
-
   render() {
-    return (
-      <form className="chat-input" onSubmit={this.submitHandler}>
-        <input type="text"
-          onChange={this.textChangeHandler}
-          value={this.state.chatInput}
-          placeholder="Write a message..."
-          required />
-      </form>
-    );
+    // Display a user input form and do something when it is submitted
   }
 }
 
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,35 +2,16 @@ import React from 'react';
 
 import Message from './Message';
 
+// This is the main display of the application. It shows a list of all the
+// messages which have been sent and received during the current chat session.
 class Messages extends React.Component {
-  componentDidUpdate() {
-    // There is a new message in the state, scroll to bottom of list
-    const objDiv = document.getElementById('messageList');
-    objDiv.scrollTop = objDiv.scrollHeight;
-  }
-
   render() {
-    // Loop through all the messages in the state and create a Message component
-    const messages = this.props.messages.map((message, i) => {
-        return (
-          <Message
-            key={i}
-            username={message.username}
-            message={message.message}
-            fromMe={message.fromMe} />
-        );
-      });
-
-    return (
-      <div className='messages' id='messageList'>
-        { messages }
-      </div>
-    );
+    // Here we should loop through each message and
+    // pass it to the Message component
   }
 }
 
 Messages.defaultProps = {
-  messages: []
 };
 
 export default Messages;
